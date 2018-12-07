807ccb4d45ea9cc64818f2e5f4d700e3c2273e33
Abacate123 <chrystianborges2010@gmail.com>
2018-12-06 22:57:27 -0300

    Merge some changes from Unity repo

--- config.sh
+++ config.sh
@@ -37,13 +37,15 @@ LATESTARTSERVICE=false
 
 # Unity Variables
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maxium android version for your mod (note that magisk has it's own minimum api: 21 (lollipop))
+# Uncomment SEPOLICY if you have sepolicy patches in common/sepolicy.sh. Unity will take care of the rest
 # Uncomment DYNAMICOREO if you want libs installed to vendor for oreo and newer and system for anything older
 # Uncomment DYNAMICAPP if you want anything in $INSTALLER/system/app to be installed to the optimal app directory (/system/priv-app if it exists, /system/app otherwise)
 # Uncomment SYSOVERRIDE if you want the mod to always be installed to system (even on magisk)
 # Uncomment RAMDISK if you have ramdisk modifications. If you only want ramdisk patching as part of a conditional, just keep this commented out and set RAMDISK=true in that conditional.
 # Uncomment DEBUG if you want full debug logs (saved to SDCARD if in twrp, part of regular log if in magisk manager (user will need to save log after flashing)
-MINAPI=21
-MAXAPI=28
+#MINAPI=21
+#MAXAPI=25
+#SEPOLICY=true
 #SYSOVERRIDE=true
 #DYNAMICOREO=true
 #DYNAMICAPP=true
@@ -64,10 +66,14 @@ unity_custom() {
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Greenify4Magisk Module     "
-  ui_print "              By Abacate123               "
-  ui_print "*******************************"
+  ui_print " "
+  ui_print "    *******************************************"
+  ui_print "    *<name>*"
+  ui_print "    *******************************************"
+  ui_print "    *<version>*"
+  ui_print "    *<author>*"
+  ui_print "    *******************************************"
+  ui_print " "
 }
 
 ##########################################################################################
